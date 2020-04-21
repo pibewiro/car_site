87ca0e1e4e6a5f503609a016fff033638845331b
prince <princeibewiro@indecex.com.br>
2020-04-20 21:23:18 -0300

    upd

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, 'dist', 'index.html')));
+app.use("/", express.static(path.join(__dirname, 'dist')));
+app.use("/dist", express.static(path.join(__dirname, 'css')));
+
 let port = process.env.PORT || 8080;
 app.listen(port);
 console.log("Listening to port:", port);
