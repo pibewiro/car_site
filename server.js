152a74ba6b9873d53e73a4ab3e2b151c03155b94
prince <princeibewiro@indecex.com.br>
2020-04-20 21:57:15 -0300

    upd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const serverStatic = require("server-static");
+const serverStatic = require("serve-static");
 
 const app = express();
 
