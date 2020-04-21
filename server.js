a3bd4ca9dab314f6ea844199b145aff81f336ef1
prince <princeibewiro@indecex.com.br>
2020-04-21 00:29:21 -0300

    u

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const serverStatic = require("serve-static");
-
+const history = require('connect-history-api-fallback');
 const app = express();
+
+app.use(history());
 
 app.use("/", serverStatic(path.join(__dirname, "dist")));
 
